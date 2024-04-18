7ca05f2686840e410c742758acb022d79c7d2006
Jane <hezha.aban@gmail.com>
2024-04-18 18:04:48 +0200

    minut 1:47:40

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -13,6 +13,8 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
     private readonly UserManager<UserEntity> _userManager = userManager;
     private readonly SignInManager<UserEntity> _signInManager = signInManager;
     private readonly ApplicationContext _context = context;
+
+    [Route("/signup")]
     public IActionResult SignUp()
     {
         return View();
@@ -21,6 +23,7 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
 
 
     [HttpPost]
+    [Route("/signup")]
     public async Task<IActionResult> SignUp(SignUpViewModel model)
     {
 
@@ -63,7 +66,7 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
     }
 
 
-
+    [Route("/signin")]
     public IActionResult SignIn(string returnUrl)
     {
         ViewData["ReturnUrl"] = returnUrl ?? "/"; //är return null så får den en "/" slash.
@@ -73,6 +76,7 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
 
 
     [HttpPost]
+    [Route("/signin")]
     public async Task<IActionResult> SignIn(SignInViewModel model, string returnUrl)
     {
         if (ModelState.IsValid)
@@ -86,7 +90,7 @@ public class AuthController(UserManager<UserEntity> userManager, SignInManager<U
         return View(model);
     }
 
-
+    [Route("/signout")]
     public new async Task<IActionResult> SignOut()
     {
         await _signInManager.SignOutAsync();
