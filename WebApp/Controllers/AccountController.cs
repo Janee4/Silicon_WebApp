8c1edcb097820693f3105767f30876797fdd370a
Jane <hezha.aban@gmail.com>
2024-04-19 16:36:58 +0200

    gjort klart 2:24:19 Account Controllers med try catch

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Infrastructure.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
@@ -70,24 +72,39 @@ public class AccountController : Controller
 
         if (TryValidateModel(model.Adress!))
         {
-            var user = await _userManager.GetUserAsync(User);
+            var nameIdentifier = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            var user = await _context.Users.Include (i=> i.Adress).FirstOrDefaultAsync(x => x.Id == nameIdentifier);
             if (user != null)
             {
-                user.FirstName = user.FirstName;
-                user.LastName = user.LastName;
-                user.Email = user.Email;
-                user.PhoneNumber = user.PhoneNumber;
-                user.UserName = user.UserName;
-                user.Bio = user.Bio;
-
-                var result = await _userManager.UpdateAsync(user);
-                if (result.Succeeded)
+                try
                 {
+                    if (user.Adress != null)
+                    {
+                        user.Adress.AdressLine_1 = model.Adress!.AdressLine_1;
+                        user.Adress.AdressLine_2 = model.Adress!.AdressLine_2;
+                        user.Adress.PostalCode = model.Adress!.PostalCode;
+                        user.Adress.City = model.Adress!.City;
+                    }
+                    else
+                    {
+                        user.Adress = new AdressEntity
+                        {
+                            AdressLine_1 = model.Adress!.AdressLine_1,
+                            AdressLine_2 = model.Adress!.AdressLine_2,
+                            PostalCode = model.Adress!.PostalCode,
+                            City = model.Adress!.City
+                        };
+                    }
+
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
                     TempData["StatusMessage"] = "Updated basic address successfully.";
                 }
-                else
+
+                catch
                 {
                     TempData["StatusMessage"] = "Unable to save address information.";
+
                 }
 
             }
